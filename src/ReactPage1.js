e704cbc3280469e40b9f0dfbb9d002e70d27c3d5
周薇-20670 <wei.zhou05@hand-china.com>
2018-08-03 09:24:31 +0800

    zw

---
+++ src/ReactPage1.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+class Home extends React.Component {
+  
+    render(){
+      return <div>
+            <h2>首页</h2> 
+            <div>
+              <p>balabalbalbalbablablabal...</p>
+              <p>balabalbalbalbablablabal...</p>
+              <p>balabalbalbalbablablabal...</p>
+            </div>
+       </div>;
+    }
+  }
+  
+
+
+class AboutMe extends React.Component {
+  
+    render(){
+      return <div>
+            <h2>关于我</h2> 
+            <div>
+              <p>bababalb...</p>
+              <p>323...</p>
+              <p>2323...</p>
+            </div>
+       </div>;
+    }
+  }
+  
+
+  class OhterPage extends React.Component {
+  
+    render(){
+      return <div>
+            <h2>其他页面</h2> 
+            <div>
+              <p>阿斯顿发生的爱上d...</p>
+              <p>撒的啊是的 ...</p>
+              <p>撒的啊是的 ...</p>
+            </div>
+       </div>;
+    }
+  }
+  
+
+  
+   
+  class App2 extends React.Component {
+  
+    render(){
+      return (
+        <div>
+            <ul>
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+            </ul>
+            <div>
+                <Home />
+                <AboutMe />
+                <OhterPage />
+            </div>
+        </div>
+      );
+    }
+  }
+
+  export default App2;
+  
---
+++ src/ReactPage2.js
@@ -0,0 +1,352 @@
+import React from 'react';
+import './style.less';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch
+  }   from 'react-router-dom';
+import GameApp from './GameApp';
+  
+  class Home extends React.Component {
+    
+    render(){
+      return <div>
+            <h1>欢迎来到研发中心培训网站</h1>
+            <button>文档</button>
+            <button>日报</button>
+       </div>;
+    }
+  }
+
+  class Myhou extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约1</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+
+  class Myqian extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约2</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+
+  class Myyi extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约3</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+  class Myti extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约4</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+  class Myrong extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约5</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+  class Myce extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约6</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+  class day extends React.Component {
+    
+    render(){
+      return (
+        <div className="about-me-wrap">
+                <div className="container">
+                  <div className="side">
+                    <ul>
+                      <li><Link to="/other/hou">后端</Link></li>
+                      <li><Link to="/other/qian">前端</Link></li>
+                      <li><Link to="/other/yi">移动中心</Link></li>
+                      <li><Link to="/other/ti">提前批</Link></li>
+                      <li><Link to="/other/rong">融晶</Link></li>
+                      <li><Link to="/other/ce">测试中心</Link></li>
+                    </ul>
+                   </div>
+                  <div className="main">
+                    <Switch>
+                     <Route path="/other/hou" component={Myhou}/>
+                     <Route path="/other/qian" component={Myqian}/>
+                     <Route path="/other/yi" component={Myyi}/>
+                     <Route path="/other/ti" component={Myti}/>
+                     <Route path="/other/rong" component={Myrong}/>
+                     <Route path="/other/ce" component={Myce}/>
+                     <Route component={Myhou}/>
+                    </Switch>
+                  </div>
+                </div>
+           </div>
+      );
+    }
+  }
+  
+  class Myone extends React.Component {
+
+    render(){
+      return <div>
+            <h2>建表规约</h2>
+            <h5>强制要求</h5>
+            <div>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+              <p>表达是与否概念的字段，必须使用 is_xxx 的方式命名，数据类型是 unsigned tinyint （1 表示是， 0 表示否）。</p>
+              <p>表名、字段名必须使用小写字母或数字，禁止出现数字开头，禁止两个下划线中间只出现数字。</p>
+              <p>表名不使用复数名词</p>
+            </div>
+       </div>;
+    }
+  }
+  
+  class Mytwo extends React.Component {
+    
+    render(){
+      return <div>
+            <h2>java后端开发规范</h2>
+            <h5>规范检查</h5>
+            <div>
+              <p>本地开发开启两种规范检查，一种为sonarQube的规范检查，一种为基于sun和google的checkStyle规范检查。</p>
+              <p>同时在CI的SonarQube代码检查中也生效了这两种规范检查，控制代码质量。</p>
+              <p>本地开发开启两种规范检查，一种为sonarQube的规范检查，一种为基于sun和google的checkStyle规范检查。</p>
+              <p>同时在CI的SonarQube代码检查中也生效了这两种规范检查，控制代码质量。</p><p>本地开发开启两种规范检查，一种为sonarQube的规范检查，一种为基于sun和google的checkStyle规范检查。</p>
+              <p>同时在CI的SonarQube代码检查中也生效了这两种规范检查，控制代码质量。</p>
+              <p>本地开发开启两种规范检查，一种为sonarQube的规范检查，一种为基于sun和google的checkStyle规范检查。</p>
+              <p>同时在CI的SonarQube代码检查中也生效了这两种规范检查，控制代码质量。</p>
+            </div>
+       </div>;
+    }
+  }
+  
+  
+  class Mythree extends React.Component {
+    
+    render(){
+      return <div>
+            <h1>服务划分</h1> 
+            <h1>单个整体架构设计</h1> 
+       </div>;
+    }
+  }
+
+  class Myfour extends React.Component {
+    
+    render(){
+      return <div>
+            <h1>Rest api 规范</h1>
+            <div>
+              <p>Fielding是一个非常重要的人，他是HTTP协议（1.0版和1.1版）的主要设计者、Apache服务器软件的作者之一、Apache基金会的第一任主席。Fielding将他对互联网软件的架构原则，定名为 REST，即 Representational State Transfer 的缩写。</p>
+            </div>
+            <h1>RESTful API Design 名词定义</h1>
+            <div>
+              <p>Fielding是一个非常重要的人，他是HTTP协议（1.0版和1.1版）的主要设计者、Apache服务器软件的作者之一、Apache基金会的第一任主席。Fielding将他对互联网软件的架构原则，定名为 REST，即 Representational State Transfer 的缩写。</p>
+            </div> 
+       </div>;
+    }
+  }
+  class Myfive extends React.Component {
+    
+    render(){
+      return <div>
+            <h1>前端规范</h1> 
+            <div>
+              <p>HTML 使用语义化标签，禁止滥用Div，Span</p>
+              <p>HTML 使用语义化标签，禁止滥用Div，Span</p>
+              <p>HTML 使用语义化标签，禁止滥用Div，Span</p>
+              <p>HTML 使用语义化标签，禁止滥用Div，Span</p>
+              <p>HTML 使用语义化标签，禁止滥用Div，Span</p>
+              <p>HTML 使用语义化标签，禁止滥用Div，Span</p>
+            </div> 
+       </div>;
+    }
+  }
+
+  class Mysix extends React.Component {
+    
+    render(){
+      return <div>
+            <h1>Git 提交规范</h1> 
+            <h2>首次拉代码时，需配置如下信息</h2> 
+       </div>;
+    }
+  }
+  
+  class Myseven extends React.Component {
+    
+    render(){
+      return <div>
+            <h1>Chart目录</h1> 
+            <p>每个需要进行部署的应用都需要有chart</p>
+            <h1>Chart目录</h1> 
+            <p>每个需要进行部署的应用都需要有chart</p> 
+            <h1>Chart目录</h1> 
+            <p>每个需要进行部署的应用都需要有chart</p> 
+            <h1>Chart目录</h1> 
+            <p>每个需要进行部署的应用都需要有chart</p> 
+            <h1>Chart目录</h1> 
+            <p>每个需要进行部署的应用都需要有chart</p>  
+       </div>;
+    }
+  }
+  
+  class AboutMe extends React.Component {
+    
+    render(){
+      return (
+            <div className="about-me-wrap">
+                <a href="#" className="same">编码规范</a> 
+                <div className="container">
+                  <div className="side">
+                    <ul>
+                      <li><Link to="/about/one">SQL规范</Link></li>
+                      <li><Link to="/about/two">Java基础开发规范</Link></li>
+                      <li><Link to="/about/three">Java后端服务开发规范</Link></li>
+                      <li><Link to="/about/four">Rest api 规范</Link></li>
+                      <li><Link to="/about/five">前端服务开发规范</Link></li>
+                      <li><Link to="/about/six">Git 提交规范</Link></li>
+                      <li><Link to="/about/seven">Gitlab CI规范</Link></li>
+                    </ul>
+                   </div>
+                  <div className="main">
+                    <Switch>
+                     <Route path="/about/one" component={Myone}/>
+                     <Route path="/about/two" component={Mytwo}/>
+                     <Route path="/about/three" component={Mythree}/>
+                     <Route path="/about/four" component={Myfour}/>
+                     <Route path="/about/five" component={Myfive}/>
+                     <Route path="/about/six" component={Mysix}/>
+                     <Route path="/about/seven" component={Myseven}/>
+                     <Route component={Mythree}/>
+                    </Switch>
+                  </div>
+                </div>
+           </div>
+      );
+    }
+  }
+
+  
+  class App extends React.Component {
+    
+   render(){
+      return (
+          <HashRouter>
+            <div className="app-wrap">
+              <ul className="nav-header">
+                <li><Link to="/index">Coperonom</Link></li>
+                <li><Link to="/other" className="nav-right1">每日总结</Link></li>
+                <li><Link to="/about" className="nav-right2">文档</Link></li>
+              </ul>
+              <div className="app-main">
+                <Switch>
+                  <Route path="/index" component={Home}/>
+                  <Route path="/about" component={AboutMe} />
+                  <Route path="/other" component={day}/>
+                  <Route component={AboutMe}/>
+                </Switch>
+              </div>
+            </div>
+        </HashRouter>
+      );
+   }
+  }
+  
+
+export default App;
+//   ReactDOM.render(<App />,  document.getElementById('container'));
